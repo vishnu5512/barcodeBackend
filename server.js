05f0feaebffec849463f6e1b669b9c133c4bd51c
Suresh300395 <nagagangasureshp@adityauniversity.in>
2026-03-28 14:25:55 +0530

    v8

--- server.js
+++ server.js
@@ -8,6 +8,10 @@ const app = express();
 app.use(require("cors")());
 app.use(express.json());
 
+app.get("/", (req, res) => {
+    res.json({ status: "alive", message: "Barcode Backend is running!" });
+});
+
 const clients = {};
 const jobs = {};
 
